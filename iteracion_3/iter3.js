7529c573df828459cdd8213842ed89f03e74c517
Toni M <tonims.dev@gmail.com>
2021-12-09 21:12:00 +0100

    Iteration#3

---
+++ iteracion_3/iter3.js
@@ -0,0 +1,15 @@
+//3.operadores
+//1.1
+console.log(10 * 5);
+//1.2
+console.log(10 / 2);
+//1.3
+console.log(15 % 9);
+//1.4
+let y = 10;
+let z = 5;
+let x = (y + z);
+console.log(x);
+//1.5
+let n = (y * z);
+console.log(n);
