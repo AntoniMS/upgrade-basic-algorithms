9ae2cdb4aa4dc4971008216a8cb86045afe20f37
Toni M <tonims.dev@gmail.com>
2021-12-09 22:41:37 +0100

    Iteration#6

---
+++ iteracion_6/iter6.js
@@ -0,0 +1,20 @@
+//6. bucles
+//1.1
+for (let i = 0; i <= 9; i++) {
+    console.log("1.1 - ", i);
+}
+
+//1.2
+for (let j = 0; j <= 9; j++) {
+    if(j % 2 == 0) {
+    console.log("1.2 - ", j);
+    }
+}
+//1.3
+for (let k = 0; k <= 9; k++)  {
+    if (k != 9) {
+        console.log("Intentando dormir...");
+    } else {
+        console.log("Dormido!")
+    }
+}
